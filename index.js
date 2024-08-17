be265f1477ca2f54a1caaeff0f985d9520b70f3b
Rafidul01 <rafidulhasan555@gmail.com>
2024-08-17 12:16:12 +0600

    soting added

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hjxwn6k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,51 +16,59 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-
     const productCollection = client.db("productHouse").collection("products");
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
-
     app.get("/products", async (req, res) => {
-      const brand = req.query.brand; 
+      const brand = req.query.brand;
       const category = req.query.category;
       const priceRange = req.query.price;
+      const sortOption = req.query.sort;
       let query = {};
-    
+
       if (brand) {
-        
-        query = { brandName: brand }; 
-        
+        query = { brandName: brand };
       }
       if (category) {
-        
-          query.categoryName = category; 
-        
+        query.categoryName = category;
       }
-      if (priceRange && priceRange !== 'price') {
-        const [minPrice, maxPrice] = priceRange.split('-').map(Number);
-        
-        if (maxPrice === 'infinity') {
-          query.price = { $gte: minPrice };  
+      if (priceRange && priceRange !== "price") {
+        const [minPrice, maxPrice] = priceRange.split("-").map(Number);
+
+        if (maxPrice === "infinity") {
+          query.price = { $gte: minPrice };
         } else {
-          query.price = { $gte: minPrice, $lte: maxPrice };  
+          query.price = { $gte: minPrice, $lte: maxPrice };
         }
       }
 
-    
-      const cursor = productCollection.find(query); 
-      const result = await cursor.toArray();
+      let sortQuery = {};
+
+      if (sortOption) {
+        if (sortOption === "Low to High") {
+          sortQuery.price = 1; // Ascending order
+        } else if (sortOption === "High to Low") {
+          sortQuery.price = -1; // Descending order
+        } else if (sortOption === "Newest first") {
+          sortQuery.creationDate = -1; // Newest first
+        }
+      }
+
+      const cursor = productCollection.find(query).sort(sortQuery);
+  const result = await cursor.toArray();
       res.send(result);
     });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -70,8 +76,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get("/", (req, res) => {  
+app.get("/", (req, res) => {
   res.send("server is running ..... :) ");
 });
 
